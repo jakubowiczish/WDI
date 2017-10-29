a70c4c41713005e2e8ea312f832af9e0452f793c
jakubowiczish <jakub77plotnikowski@gmail.com>
2017-10-29 23:59:13 +0100

    dodano 1 plik

---
+++ Czy liczba jest sumą co najwyzej 4 kolejnych liczb fibonacciego/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned long long k;
+    cin>>k;
+
+    unsigned long long f = 2, fprev = 1, fprevprev = 1, fprevprevprev = 0;
+
+    // sumy co najwyzej 4 liczb fibonacciego
+    unsigned long long sum, sum2, sum3, sum4;
+
+    bool odp = false;
+
+    while (f <= k)
+    {
+        // aktualizacja sum
+
+        sum = f + fprev + fprevprev + fprevprevprev;
+        sum2 = f + fprev + fprevprev;
+        sum3 = f + fprev;
+        sum4 = f;
+
+        // cout<<"sumy po kolei: "<<sum<<" "<<sum2<<" "<<sum3<<" "<<sum4<<endl;
+
+        if((k == sum) || (k == sum2) || (k == sum3) || (k == sum4))
+        {
+            cout<<"TAK"<<endl;
+            odp = true;
+            break;
+        }
+
+        unsigned long long next = f + fprev; // wyliczanie kolejnego wyrazu
+
+        //aktualizacja wyrazow
+
+        fprevprevprev = fprevprev;
+        fprevprev = fprev;
+        fprev = f;
+        f = next;
+    }
+
+    if(odp == false)
+        cout<<"NIE"<<endl;
+
+    return 0;
+}
